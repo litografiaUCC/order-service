d1c2aaebf72551b28e78839bc588806843ee6106
Cardona-JE <juancardonaa0421@gmail.com>
2024-04-30 01:22:46 -0500

    Add the @ManyToOne annotation that was missing

--- src/main/java/com/litografiaartesplanchas/orderservice/model/Service.java
+++ src/main/java/com/litografiaartesplanchas/orderservice/model/Service.java
@@ -9,6 +9,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
@@ -32,6 +33,7 @@ public class Service {
 	@Column(name = "foto", nullable = true, length = 255)
 	private String picture;
 
+    @ManyToOne
     @JoinColumn(name = "id_tipo_servicio", nullable = false)
 	private TypeService typeService;
 
